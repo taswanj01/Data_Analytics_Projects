3317a4609b6aa409a0832fd7db41fcaef5edab12
Tai Johnson <taswanjohnson78@gmail.com>
2019-05-19 20:15:57 -0600

    Finishing Mars web scraping project

--- Module_11_Tai_Johnson_Web_Scraping/app_files/app.py
+++ Module_11_Tai_Johnson_Web_Scraping/app_files/app.py
@@ -3,7 +3,6 @@ from flask_pymongo import PyMongo
 import mars_scrape
 
 app = Flask(__name__)
-app.static_folder = 'static'
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data_app"
