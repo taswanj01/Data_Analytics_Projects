c72eea6f2825898ca9ed112a8c81ce87ce72cd69
Tai Johnson <taswanjohnson78@gmail.com>
2019-05-27 23:17:26 -0600

    Bare minimums for JS Web project

--- Module_13_Tai_Johnson_JavaScript/static/js/app.js
+++ Module_13_Tai_Johnson_JavaScript/static/js/app.js
@@ -1,32 +1,53 @@
 var filter_date = "";
-var date_conversion = "";
 
-data.forEach((object) => {
-    date_conversion = object.datetime.split("/");
-    if(parseInt(date_conversion[1]) < 10){date_conversion[1] = "0" + date_conversion[1]};
-    if(parseInt(date_conversion[0]) < 10){date_conversion[0] = "0" + date_conversion[0]};
-    object.datetime = date_conversion[2] + "-" + 
-        date_conversion[0] + "-" + date_conversion[1];
-});
+date_conversion();
 
-var k = '<tbody>'
-data.forEach((object) => {
-    k += '<tr>';
-    k += '<td>' + object.datetime + '</td>';
-    k += '<td>' + object.city + '</td>';
-    k += '<td>' + object.state + '</td>';
-    k += '<td>' + object.country + '</td>';
-    k += '<td>' + object.shape + '</td>';
-    k += '<td>' + object.durationMinutes + '</td>';
-    k += '<td>' + object.comments + '</td>';
-    k += '</tr>';
-});
-k += '</tbody>';
+var k = '<tbody>';
+all_data();
 
-afterThead = document.getElementsByTagName('thead');
-afterThead[0].insertAdjacentHTML("afterend", k);
+filter_date = document.getElementById("datetime").value;
+console.log(`filter date is ${filter_date}`);
 
 
+
+
+//########      HELPER FUNCTIONS       ########//
+
+
+// Helper function for loading all data
+function all_data(){
+    data.forEach((object) => {
+        k += '<tr>';
+        k += '<td>' + object.datetime + '</td>';
+        k += '<td>' + object.city + '</td>';
+        k += '<td>' + object.state + '</td>';
+        k += '<td>' + object.country + '</td>';
+        k += '<td>' + object.shape + '</td>';
+        k += '<td>' + object.durationMinutes + '</td>';
+        k += '<td>' + object.comments + '</td>';
+        k += '</tr>';
+    });
+    k += '</tbody>';
+    
+    afterThead = document.getElementsByTagName('thead');
+    afterThead[0].insertAdjacentHTML("afterend", k);
+};
+
+
+// Helper function for converting dates into ISO format
+function date_conversion(){
+    data.forEach((object) => {
+        var date_conversion = object.datetime.split("/");
+        if(parseInt(date_conversion[1]) < 10){date_conversion[1] = "0" + date_conversion[1]};
+        if(parseInt(date_conversion[0]) < 10){date_conversion[0] = "0" + date_conversion[0]};
+        object.datetime = date_conversion[2] + "-" + 
+            date_conversion[0] + "-" + date_conversion[1];
+    });
+};
+
+
+// Helper function that displays the data based on the users input in the date picker field
+// resets to show all data if date picker is reset
 function filter_by_date(filter_date) {
     filter_date = document.getElementById("datetime").value;
     console.log(filter_date);
